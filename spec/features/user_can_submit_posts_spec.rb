d65358de3e22bd10d82afaed43552f746dc579b2
Squareben1 <bengittins@live.com>
2020-02-14 10:34:01 +0000

    added user ID to posts

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -5,13 +5,9 @@ require 'rails_helper'
 RSpec.feature 'Timeline', type: :feature do
   scenario 'Can submit posts and view posts, date, time and username' do
     sign_up
-
-    visit '/posts'
-    click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
+    new_post
     expect(page).to have_content('Hello, world!')
-    expect(page).to have_content('iluvcatz123')
+    expect(page).to have_content(User.all.first.id) #'cats@cats.com'
     expect(page).to have_content(Time.now.strftime('%m/%d/%Y, %H:%M'))
   end
 
@@ -25,7 +21,7 @@ RSpec.feature 'Timeline', type: :feature do
     fill_in 'Message', with: @long_text
     click_button 'Submit'
     expect(page).to have_content(@short_text)
-    expect(page).to have_content('iluvcatz123')
+    expect(page).to have_content(User.all.first.id) #'cats@cats.com'
     expect(page).to have_content(Time.now.strftime('%m/%d/%Y, %H:%M'))
   end
 end
